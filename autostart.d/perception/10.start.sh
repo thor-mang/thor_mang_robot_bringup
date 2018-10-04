4871f473ab83c5920dbc874605e21e94110719da
perception <you@example.com>
2018-10-04 19:24:41 +0200

    fixed autostart

--- autostart.d/perception/10.start.sh
+++ autostart.d/perception/10.start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # wait for roscore
-bash -ic "export ROS_MASTER_URI=http://thor-motion:11311; export ROS_IP=192.168.1.11; thor wait_for_roscore"
+bash -ic "export ROS_MASTER_URI=http://motion:11311; export ROS_IP=192.168.1.11; thor wait_for_roscore"
 
 # run perception start command
 bash -ic "thor perception start"
